9f5b19e79d3d2cc95d465216f5b2c13693ef1565
Abir Sarkar <abirsarkar1999@gmail.com>
2023-11-11 09:19:58 +0000

    day 2 datatypes

---
+++ 01-starting-js/dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // trests all the code as newer version of javascript code
+
+
+// let foo = 42; // foo is now a number
+foo = "bar"; // foo is now a string
+// foo = true; // foo is now a boolean
+
+
+console.log(foo);

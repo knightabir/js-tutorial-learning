8ac99028d8db09bd24af59a29116b67959113c83
Abir Sarkar <abirsarkar1999@gmail.com>
2023-11-11 09:18:54 +0000

    Day 2 datatypes

---
+++ 01-starting-js/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 13242
+let accountEmail = "abir@gmail.com"
+var accountPassword = "12342"
+accountCity = "Kolkata"
+
+console.log(accountId)
+
+accountId = 321412
+
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)

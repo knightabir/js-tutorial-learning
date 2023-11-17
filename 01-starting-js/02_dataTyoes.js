5fc7ca62859b4e1b5d8e2c8cc2e43286ab4d6b0d
Abir Sarkar <abirsarkar1999@gmail.com>
2023-11-17 12:50:09 +0000

    2nd day of learning

---
+++ 01-starting-js/02_dataTyoes.js
@@ -0,0 +1,14 @@
+"use strict"; // trat all js code as newer version
+
+// alert("hello") // we are using nodejs ,not browser;
+
+// console.log(3 + 3);
+// console.log(4 + 6);
+
+
+// window.alert("hello world")
+// alert("hello World")
+console.log(!(true|false));
+
+// alert()
+// alert("hello world")
--- 01-starting-js/dataTypes.js
+++ 01-starting-js/dataTypes.js
@@ -1,9 +1,7 @@
 "use strict"; // trests all the code as newer version of javascript code
 
-
 // let foo = 42; // foo is now a number
 foo = "bar"; // foo is now a string
 // foo = true; // foo is now a boolean
-
 
 console.log(foo);

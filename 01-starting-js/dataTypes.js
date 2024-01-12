d4f14edc51d3194566e7cabc7dd3f3a8568989c7
Abir Sarkar <abirsarkar1999@gmail.com>
2024-01-12 08:10:00 +0000

    "String Video Done"

--- 01-starting-js/dataTypes.js
+++ 01-starting-js/dataTypes.js
@@ -1,7 +1,33 @@
-"use strict"; // trests all the code as newer version of javascript code
+// "use strict"; // trests all the code as newer version of javascript code
 
 // let foo = 42; // foo is now a number
-foo = "bar"; // foo is now a string
+// foo = "bar"; // foo is now a string
 // foo = true; // foo is now a boolean
 
-console.log(foo);
+// console.log(foo);
+
+
+
+//Stack memory (Primitive) ,Heap Memory (Non-Primitive)
+
+let myName = "Abir Sarkar"
+
+let anotherName = myName
+
+anotherName = "Sarkar Abir"
+
+
+console.log(anotherName);
+console.log(myName);
+
+let user1 = {
+    email:"abirsarkar1999@gmail.com",
+    upi:"9641598284@axl"
+}
+
+let user2 = user1
+
+user2.email="newemail@gmail.com"
+
+console.log(user2)
+console.log(user1);
---
+++ 01-starting-js/strings.js
@@ -0,0 +1,9 @@
+const myName = "Abir Sarkar"
+const repoCount = 50
+
+
+// console.log(`Hi My name is : ${myName} and my repo count is : ${repoCount}`)
+
+const a = new String('My name is Abir Sarkar')
+console.log(typeof(a));
+console.log(typeof(myName));

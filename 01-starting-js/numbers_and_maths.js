e61ddf612ef5baf16ab4b12266e025c4a695c60a
Abir Sarkar <abirsarkar1999@gmail.com>
2024-01-13 17:44:04 +0000

    numbers and maths

---
+++ 01-starting-js/numbers_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+const balance = new Number(400)
+
+// console.log(balance);
+// console.log(score);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.5487
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'))
+

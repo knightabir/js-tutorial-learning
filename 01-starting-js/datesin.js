be4c5a5663495076bc64020b43cfc35fc41c2a62
Abir Sarkar <abirsarkar1999@gmail.com>
2024-01-28 13:27:53 +0000

    Time lecture compmlete

---
+++ 01-starting-js/datesin.js
@@ -0,0 +1,24 @@
+//dates
+
+// let date = new Date()
+// console.log(typeof date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+
+// console.log(date.toJSON());
+
+// console.log(date.toLocaleDateString());
+
+let myDate = new Date(2023,0,23);
+// console.log(myDate.toLocaleString());
+// let myTimeStamp = Date.now()
+// console.log(myDate.getTime());
+// console.log(Math.round(Date.now()/1000));
+
+
+
+let newDate = new Date()
+
+console.log(newDate.toLocaleString());
+console.log(newDate.getDay());
